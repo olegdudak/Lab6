1bfe767592c96688eb6f88b305cf5be1e73c6d8f
Oleh <nicely.group.front1@gmail.com>
2025-12-10 23:13:15 +0200

    Fix static files configuration to remove warning

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,9 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
+# Only include STATICFILES_DIRS if the directory exists
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
